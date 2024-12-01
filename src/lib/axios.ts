3fffe5cc731fc3273950ae792b015cd9e90ea5d3
mskomalraut <komalraut1209@gmail.com>
2024-12-01 21:38:51 +0530

    changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://spotify-backend-3euu.onrender.com/api",
 });
 
